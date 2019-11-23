cc519711b3de65eb7b332b9d3141bf72089d49ba
Hiroaki Yutani <yutani.ini@gmail.com>
2019-11-23 16:55:07 +0900

    Initial commit

---
+++ example1/src/main.rs
@@ -0,0 +1,43 @@
+use nannou::prelude::*;
+
+fn main() {
+    nannou::app(model).update(update).run();
+}
+
+struct Model;
+
+fn model(app: &App) -> Model {
+    app.new_window().event(event).view(view).build().unwrap();
+}
+
+fn update(app: &App, _model: &mut Model, _update: Update) {}
+
+fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
+    println!("{:?}", event);
+
+    match event {
+        KeyPressed(_key) => {}
+        KeyReleased(_key) => {}
+        MouseMoved(_pos) => {}
+        MousePressed(_button) => {}
+        MouseReleased(_button) => {}
+        MouseWheel(_amount, _phase) => {}
+        MouseEntered => {}
+        MouseExited => {}
+
+        Touch(_touch) => {}
+        TouchPressure(_pressure) => {}
+        Moved(_pos) => {}
+        Resized(_size) => {}
+        HoveredFile(_path) => {}
+        DroppedFile(_path) => {}
+        HoveredFileCancelled => {}
+        Focused => {}
+        Unfocused => {}
+        Closed => {}
+    }
+}
+
+fn view(_app: &App, _model: &Model, frame: &Frame) {
+    frame.clear(DIMGRAY);
+}
