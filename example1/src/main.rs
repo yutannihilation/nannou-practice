257b85cb54caaafac32e231e4ff54670ca7952dd
Hiroaki Yutani <yutani.ini@gmail.com>
2020-04-25 14:44:12 +0900

    Update

--- example1/src/main.rs
+++ example1/src/main.rs
@@ -12,13 +12,23 @@ fn model(_app: &App) -> Model {
 
 fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw();
+    
+    let pad = 25.0;
+    let win = app.window_rect();
+    let win_p = win.pad(pad);
 
-    draw.background().color(BLUE);
+    let square = Rect::from_w_h(100.0, 100.0).top_left_of(win_p);
 
     draw.rect()
-        .x_y(0.0, 0.0)
-        .w_h(100.0, 100.0)
+        .xy(square.xy())
+        .wh(square.wh())
         .color(PLUM);
 
+    let circle = square.below(square).shift_y(-pad);
+    draw.ellipse()
+        .xy(circle.xy())
+        .wh(circle.wh())
+        .color(SALMON);
+    
     draw.to_frame(app, &frame).unwrap();
 }
