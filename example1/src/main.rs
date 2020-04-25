e36931547047d5c9af5d5dd98e3f2e692cfbda57
Hiroaki Yutani <yutani.ini@gmail.com>
2020-04-25 13:32:32 +0900

    Update

--- example1/src/main.rs
+++ example1/src/main.rs
@@ -1,44 +1,24 @@
 use nannou::prelude::*;
 
 fn main() {
-    nannou::app(model).update(update).run();
+    nannou::app(model).simple_window(view).run();
 }
 
-struct Model;
+struct Model {}
 
-fn model(app: &App) -> Model {
-    app.new_window().event(event).view(view).build().unwrap();
-    Model
+fn model(_app: &App) -> Model {
+    Model {}
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn view(app: &App, _model: &Model, frame: Frame) {
+    let draw = app.draw();
 
-fn event(_app: &App, _model: &mut Model, event: WindowEvent) {
-    println!("{:?}", event);
+    draw.background().color(BLUE);
 
-    match event {
-        KeyPressed(_key) => {}
-        KeyReleased(_key) => {}
-        MouseMoved(_pos) => {}
-        MousePressed(_button) => {}
-        MouseReleased(_button) => {}
-        MouseWheel(_amount, _phase) => {}
-        MouseEntered => {}
-        MouseExited => {}
+    draw.rect()
+        .x_y(0.0, 0.0)
+        .w_h(100.0, 100.0)
+        .color(PLUM);
 
-        Touch(_touch) => {}
-        TouchPressure(_pressure) => {}
-        Moved(_pos) => {}
-        Resized(_size) => {}
-        HoveredFile(_path) => {}
-        DroppedFile(_path) => {}
-        HoveredFileCancelled => {}
-        Focused => {}
-        Unfocused => {}
-        Closed => {}
-    }
-}
-
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    frame.clear(DIMGRAY);
+    draw.to_frame(app, &frame).unwrap();
 }
